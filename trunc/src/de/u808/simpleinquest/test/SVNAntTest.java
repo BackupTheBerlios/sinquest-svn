bed5b65fe45092e8af97068e89cfc361ea9b15be
friedeas <friedeas@958a345e-7a58-0410-8653-9d417c531cd6>
2008-11-14 23:17:45 +0000

    build improved

---
+++ trunc/src/de/u808/simpleinquest/test/SVNAntTest.java
@@ -0,0 +1,12 @@
+package de.u808.simpleinquest.test;
+
+public class SVNAntTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.library.path"));
+	}
+
+}
