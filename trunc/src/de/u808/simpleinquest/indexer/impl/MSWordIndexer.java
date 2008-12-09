55b4a146e58f751428017dbf28b6b4950b55400d
friedeas <friedeas@958a345e-7a58-0410-8653-9d417c531cd6>
2008-12-09 01:40:36 +0000

    testing

--- trunc/src/de/u808/simpleinquest/indexer/impl/MSWordIndexer.java
+++ trunc/src/de/u808/simpleinquest/indexer/impl/MSWordIndexer.java
@@ -7,6 +7,8 @@ import java.io.StringReader;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Index;
+import org.apache.lucene.document.Field.Store;
 import org.apache.poi.hwpf.extractor.WordExtractor;
 
 import de.u808.simpleinquest.indexer.Indexer;
@@ -17,13 +19,10 @@ public class MSWordIndexer extends AbstractIndexer {
 	
 	private WordExtractor wordExtractor;
 	
-	private StringReader stringReader;
-	
 	@Override
 	public void setContentsFild(File file) throws IndexerException {
 		try {
-			stringReader = new StringReader(this.extractContent(file));
-			this.document.add(new Field(Indexer.CONTENT_FIELD_NAME, stringReader));
+			this.document.add(new Field(Indexer.CONTENT_FIELD_NAME, this.extractContent(file), Store.NO, Index.TOKENIZED));
 		} catch (Exception e) {
 			throw new IndexerException(e.getMessage(), e);
 		}
--- trunc/src/de/u808/simpleinquest/indexer/impl/PDFIndexer.java
+++ trunc/src/de/u808/simpleinquest/indexer/impl/PDFIndexer.java
@@ -5,20 +5,35 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.pdfbox.searchengine.lucene.LucenePDFDocument;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Index;
+import org.apache.lucene.document.Field.Store;
+import org.pdfbox.pdmodel.PDDocument;
+import org.pdfbox.util.PDFTextStripper;
+
+import de.u808.simpleinquest.indexer.Indexer;
 
 public class PDFIndexer extends AbstractIndexer{
 	
-	Log log = LogFactory.getLog(this.getClass());
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	private PDFTextStripper pdfStripper = null;
+	private PDDocument doc = null; 
 
 	@Override
 	public void setContentsFild(File file) throws IndexerException {
 		try {
-			this.document = LucenePDFDocument.getDocument(file);
-			this.document = FileDocument.Document(document, file);
+			doc = PDDocument.load(file);
+			pdfStripper = new PDFTextStripper();
+			this.document.add(new Field(Indexer.CONTENT_FIELD_NAME, pdfStripper.getText(doc), Store.NO, Index.TOKENIZED));
+			doc.close();
 		} catch (IOException e) {
 			String msg = "Dokument " + file.toString() + " konnte nicht indexiert werden. Ursache: " + e.getMessage(); 
 			throw new IndexerException(msg, e);
+		}
+		finally {
+			doc = null;
+			pdfStripper = null;
 		}
 	}
 
